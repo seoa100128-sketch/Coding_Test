003ffe585abe02a5e523e8e765f1e02f0af4ecfb
seoa100128-sketch <seoa100128@gmail.com>
2026-03-28 20:23:00 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 86.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        double num11 = num1;
+        double num22 = num2;
+        int answer = (int) (num11/num22*1000);
+        return answer;
+    }
+}
